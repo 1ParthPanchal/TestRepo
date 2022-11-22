9d2a2e7267aeb051fe112737a782064e61060557
amitk <amitk@DPIS4>
2022-11-22 14:57:25 +0530

    commit changes

---
+++ Dotenetcore_Demo/Dotenetcore_Demo/Model/Student.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Dotenetcore_Demo.Model
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ? Id { get; set; }
+
+        
+        public string? FistName { get; set; }
+        public string? LastName { get; set; }
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
+        }
+        public string OrderBy { get; set; } 
+    }
+}
---
+++ Dotenetcore_Demo/Dotenetcore_Demo/Model/databasesetting.cs
@@ -0,0 +1,11 @@
+﻿namespace Dotenetcore_Demo.Model
+{
+    public class DatabaseSetting
+    {
+        public string? ConnectionString { get; set; } 
+        public string? DatabaseName { get; set; } 
+
+        public string? CollectionName { get; set; } 
+        public string? StripeApiKey { get; set; }   
+    }
+}
